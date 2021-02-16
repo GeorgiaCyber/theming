0efd0b72d6e985f25fa3178db9c70c68ac81acb6
acantrell2009 <acantrell2009@gmail.com>
2021-02-16 17:02:05 -0500

    test repointing plugin

--- resources/revealjs-plugins/revealjs-plugins.js
+++ resources/revealjs-plugins/revealjs-plugins.js
@@ -1,6 +1,6 @@
-{ src: 'revealjs-plugins/menu/menu.js', async: true },
-{ src: 'revealjs-plugins/chalkboard/chalkboard.js', async: true },
-{ src: 'revealjs-plugins/title-footer/title-footer.js', callback: function() { title_footer.initialize(); } },//not async because it may then not appear
-{ src: 'revealjs-plugins/notes-pointer/notes-pointer.js', async: true },
-{ src: 'revealjs-plugins/spotlight/spotlight.js' }, // does not work with current version of reveal.js
-{ src: 'revealjs-plugins/skip-fragments/skip-fragments.js' }
+{ src: 'https://gitlab.com/gacybercenter/theming/-/jobs/1034663755/artifacts/raw/revealjs-plugins/menu/menu.js', async: true },
+{ src: 'https://gitlab.com/gacybercenter/theming/-/jobs/1034663755/artifacts/raw/revealjs-plugins/chalkboard/chalkboard.js', async: true },
+{ src: 'https://gitlab.com/gacybercenter/theming/-/jobs/1034663755/artifacts/raw/revealjs-plugins/title-footer/title-footer.js', callback: function() { title_footer.initialize(); } },//not async because it may then not appear
+{ src: 'https://gitlab.com/gacybercenter/theming/-/jobs/1034663755/artifacts/raw/revealjs-plugins/notes-pointer/notes-pointer.js', async: true },
+{ src: 'https://gitlab.com/gacybercenter/theming/-/jobs/1034663755/artifacts/raw/revealjs-plugins/spotlight/spotlight.js' }, // does not work with current version of reveal.js
+{ src: 'https://gitlab.com/gacybercenter/theming/-/jobs/1034663755/artifacts/raw/revealjs-plugins/skip-fragments/skip-fragments.js' }
